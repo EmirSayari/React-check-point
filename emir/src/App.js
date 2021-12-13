17d443549df5590025640a0287ad343eb0489d24
Emir <sayariamir2005@gmail.com>
2021-12-13 14:08:45 +0100

    first commit

---
+++ emir/src/App.js
@@ -0,0 +1,20 @@
+
+
+import React from 'react';
+import { Container , Navbar,  } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+  <Navbar expand="lg" variant="light" bg="light">
+    <Container>
+      <Navbar.Brand href="#">Navbar</Navbar.Brand>
+    </Container>
+  </Navbar>
+</Container>
+    </div>
+  );
+};
+
+export default App;
